4065d196e3703027f524b53a3d774944b37f70a9
Aleksander Matusiak <matusiak.aleksander@gmail.com>
2016-04-16 14:01:51 +0200

    Create draft of apply function

--- bdd.hs
+++ bdd.hs
@@ -3,24 +3,25 @@ import Data.Map as Map
 
 type Value = Bool
 type Variable = Int
-type Node = Maybe Int
+type Node = Int
+type Edge = Maybe Int
 data BExp = Var Variable | Val Value | Neg BExp | And BExp BExp | Or BExp BExp deriving (Show, Eq)
 
-type Triple = (Int, Node, Node)
+type Triple = (Int, Edge, Edge)
 
 type BDD = (Map Int Triple, Map Triple Int)
 
 mk :: BDD -> Int -> Node -> Node -> (BDD, Node)
 mk bdd@(t, h) i low high
   | low == high = (bdd, low)
-  | member (i, low, high) h = (bdd, Just $ h ! (i, low, high))
+  | member (i, Just low, Just high) h = (bdd, h ! (i, Just low, Just high))
   | otherwise =
     let 
       u = (fst $ findMax t) + 1
-      newT = insert u (i, low, high) t
-      newH = insert (i, low, high) u h
+      newT = insert u (i, Just low, Just high) t
+      newH = insert (i, Just low, Just high) u h
     in
-      ((newT, newH), Just u)
+      ((newT, newH), u)
 
 
 assign :: BExp -> Variable -> Value -> BExp
@@ -32,7 +33,7 @@ assign (Neg e) x v = Neg $ assign e x v
 assign (And e1 e2) x v = And (assign e1 x v) (assign e2 x v)
 assign (Or e1 e2) x v = Or (assign e1 x v) (assign e2 x v) 
 
--- Calculates the value of the expression, where are variables have assigned values
+-- Calculates the value of the expression, where all variables have assigned values
 calculate :: BExp -> Value
 calculate (Val v) = v
 calculate (Neg e) = not $ calculate e
@@ -68,12 +69,49 @@ build b = let
 buildAux :: BExp -> BDD -> Variable -> (BDD, Node)
 buildAux b bdd@(t, h) i
   | i > maxVar b =
-    if calculate b == False then (bdd, Just 0)
-    else (bdd, Just 1)
+    if calculate b == False then (bdd, 0)
+    else (bdd, 1)
   | otherwise =
     let
       (bdd1, v0) = buildAux (assign b i False) bdd (i + 1)
       (bdd2, v1) = buildAux (assign b i True) bdd1 (i + 1)
     in mk bdd2 i v0 v1
+
+type Arr2D = Map (Node, Node) Node
+type Op = Bool -> Bool -> Bool
+
+apply :: BDD -> BDD -> Op -> Node -> Node -> (BDD, Node)
+apply b1 b2 op u1 u2 =
+  let (b, u, _) = app b1 b2 op u1 u2 (Map.empty, Map.empty) (Map.empty)
+  in (b, u)
+
+app :: BDD -> BDD -> Op -> Node -> Node -> BDD ->  Arr2D -> (BDD, Node, Arr2D)
+app bdd1@(t1, h1) bdd2@(t2, h2) op u1 u2 res g =
+  let
+    (res', u', g') =
+      if member (u1, u2) g then (res, g ! (u1, u2), g)
+      else if u1 < 2 && u2 < 2 then (res, evalOp u1 u2 op, g)
+           else if v1 == v2 then mkApp v1 low1 low2 high1 high2
+                else if v1 < v2 then mkApp v1 low1 u2 high1 u2
+                     else mkApp v2 u1 low2 u1 high2
+  in
+    (res', u', insert (u1, u2) u' g')
+  where
+    (v1, Just high1, Just low1) = t1 ! u1
+    (v2, Just high2, Just low2) = t2 ! u2
+    mkApp w a1 a2 b1 b2 =
+      let
+        (res1, w1, g1) = app bdd1 bdd2 op a1 a2 res g
+        (res2, w2, g2) = app bdd1 bdd2 op b1 b2 res1 g1
+        (res3, w3) = mk res2 w w1 w2
+      in
+        (res3, w3 , g2)
+        
+evalOp :: Int -> Int -> Op -> Int
+evalOp u1 u2 op =
+  if op b1 b2 then 1 else 0
+  where
+    b1 = u1 == 1
+    b2 = u2 == 1
 
 test = Or (Var 1) (Var 3)

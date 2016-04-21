72019c7416c0c76a94750c239b79707c57f39fdc
Aleksander Matusiak <matusiak.aleksander@gmail.com>
2016-04-21 21:15:15 +0200

    Add transitions and try of reachable algorithm

--- KnightsTour.hs
+++ KnightsTour.hs
@@ -25,3 +25,26 @@ move b1 a@(i, j) b2 b@(k, l) =
      rest = foldl (\exp (v1, v2) -> And exp (Eq (Var v1) (Var v2))) (Val True) $ zip fields1 fields2
    in And (And old new) rest
     
+transitions :: Board -> Board -> BExp
+transitions b1@(n, _) b2 =
+  let
+    positions = [((i, j), (k, l)) |
+                 i <- [0..n-1], j <- [0..n-1], k <- [0..n-1], l <- [0..n-1],
+                 ((abs (i-k) == 1 && abs (j-l) == 2) || (abs (i-k) == 2 && abs (j-l) == 1))]
+  in
+    foldl (\exp (a, b) -> Or exp $ move b1 a b2 b) (Val True) positions
+
+{-reachable :: Int -> BExp -> BDD
+reachable n start =
+  reachableAux n start (build (Val False))
+
+reachableAux :: Int -> BExp -> BDD -> BDD
+reachableAux n start last =
+  let
+    and = And (transitions (n, 0) (n, n)) last
+    m = (Val False)
+    cur = Or start m
+  in
+    if cur /= last then reachableAux n start cur
+    else return cur
+-}

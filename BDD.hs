37eca018945fc41cf1d13cea3fc46edd75064ce7
Aleksander Matusiak <matusiak.aleksander@gmail.com>
2016-05-08 13:49:34 +0200

    Add print function + add comments + improve tests

--- BDD.hs
+++ BDD.hs
@@ -1,4 +1,4 @@
-module BDD(BDD, BDDNode, BExp(..), mk, build, apply, restrict, satCount, anySat, rename) where
+module BDD(BDD, BDDNode, BExp(..), Op, mk, build, apply, restrict, satCount, anySat, rename) where
 import Data.Map.Lazy as Map
 import Data.Set as Set
 
@@ -17,22 +17,32 @@ data BExp = Var Variable
 
 type Triple = (Int, Edge, Edge)
 
+-- We introduce 2 types connected with BDD.
+-- BDD type is responsible for storing T and H table
 type BDD = (Map Int Triple, Map Triple Int)
+-- BDDNode stores BDD and the number of start node
 type BDDNode = (BDD, Node)
 
+-- Prints BDD
+print :: BDDNode -> IO ()
+print ((t, _), u) = do
+  putStrLn $ "Start: " ++ (show u)
+  putStrLn $ "T table: " ++ (show t)
+
+-- Finds node based on variable number and low and high edges 
 mk :: BDD -> Int -> Node -> Node -> BDDNode
 mk bdd@(t, h) i low high
   | low == high = (bdd, low)
   | Map.member (i, Just low, Just high) h = (bdd, h ! (i, Just low, Just high))
   | otherwise =
     let 
-      u = Map.size t + 1
+      u = Map.size t
       newT = Map.insert u (i, Just low, Just high) t
       newH = Map.insert (i, Just low, Just high) u h
     in
       ((newT, newH), u)
 
-
+-- Assigns given value to given variable in a boolean expression
 assign :: BExp -> Variable -> Value -> BExp
 assign (Val v) _ _ = Val v
 assign (Var s) x v
@@ -68,23 +78,14 @@ calculate (Eq e1 e2) =
   calculate $ And (Imp e1 e2) (Imp e2 e1)
 calculate e = error $ show e
 
--- Assuming, that no values are assigned
-maxVar :: BExp -> Int
-maxVar (Var v) = v
-maxVar (Val v) = 0
-maxVar (Neg e) = maxVar e
-maxVar (Or e1 e2) = max (maxVar e1) $ maxVar e2
-maxVar (And e1 e2) = max (maxVar e1) $ maxVar e2
-maxVar (Imp e1 e2) = max (maxVar e1) $ maxVar e2
-maxVar (Eq e1 e2) = max (maxVar e1) $ maxVar e2
-
-
-initBDD :: Variable  -> BDD
+-- Inits BDD for given number of variables with nodes for True and False
+initBDD :: Int  -> BDD
 initBDD n =
   (Map.fromList [(0, v), (1, v)], Map.empty)
   where
-    v = (n, Nothing, Nothing)
+    v = (n + 1, Nothing, Nothing)
 
+-- Builds BDD based on boolean expression and number of variables in the expression
 build :: BExp -> Int -> BDDNode
 build b n = let
   v = (n + 1, Nothing, Nothing)
@@ -105,9 +106,12 @@ buildAux b n bdd@(t, h) i
 type Arr2D = Map (Node, Node) Node
 type Op = Bool -> Bool -> Bool
 
+-- Count number of variables in BDD - this value incremented by 1 should be
+-- stored in True and False nodes
 countVariables :: BDD -> Int
-countVariables (t, _) = maximum [v | (_, (v, _, _)) <- Map.toList t]  
+countVariables (t, _) = maximum [v | (_, (v, _, _)) <- Map.toList t] - 1
 
+-- Applies given operation to 2 BDDs
 apply :: BDDNode -> BDDNode -> Op -> BDDNode
 apply (b1, u1) (b2, u2) op =
   (b, u)
@@ -117,7 +121,6 @@ apply (b1, u1) (b2, u2) op =
     n = max n1 n2
     start = initBDD n
     (b, u, _) = app b1 b2 op u1 u2 start (Map.empty)
-    
 
 app :: BDD -> BDD -> Op -> Node -> Node -> BDD ->  Arr2D -> (BDD, Node, Arr2D)
 app bdd1@(t1, h1) bdd2@(t2, h2) op u1 u2 res g =
@@ -150,6 +153,7 @@ evalOp u1 u2 op =
     b1 = u1 == 1
     b2 = u2 == 1
 
+-- Restricts BDD by assigning a value to given variable
 restrict :: BDDNode -> Variable -> Value -> BDDNode
 restrict bddNode j b =
   restrictAux bddNode
@@ -184,7 +188,7 @@ countHigh ((t, _), u) = v
   where
     (_, _, Just v) = t ! u
 
-
+-- Counts the number of solutions for given BDD
 satCount :: BDDNode -> Int
 satCount (bdd@(t, _), node) =
   2^(vNode - 1) * (count node) 
@@ -198,6 +202,7 @@ satCount (bdd@(t, _), node) =
                 +  2^(countVar (bdd, countHigh (bdd, u)) - countVar (bdd, u) - 1)
                 * count (countHigh (bdd, u))
 
+-- Calculates a solution for given BDD
 anySat :: BDDNode -> [(Variable, Value)]
 anySat (bdd, u) =
   if u == 0 then error "No truth-assignment exists"
@@ -206,10 +211,10 @@ anySat (bdd, u) =
               ((countVar (bdd, u), True) : anySat (bdd, countHigh (bdd, u)))
             else ((countVar (bdd, u), False) : anySat (bdd, countLow (bdd, u)))
 
-
 createH :: Map Int Triple -> Map Triple Int
 createH t = Map.fromList $ Prelude.map (\(a,b) -> (b,a)) $ Map.toList t
 
+-- Renames variables in BDD
 rename :: BDDNode -> [Variable] -> [Variable] -> BDDNode
 rename ((t, h), u) old new =
   let
@@ -220,8 +225,8 @@ rename ((t, h), u) old new =
     newT = mapMaybe f t
     newH = createH newT
   in ((newT, newH), u)
-  
 
+-- Auxiliary function for counting nodes in BDD
 countNodes :: BDDNode -> Int
 countNodes (bdd, u) =
   Set.size $ countNodesAux bdd u Set.empty
@@ -240,26 +245,22 @@ countNodesAux bdd@(t, _) u vis =
         (Just l) -> countNodesAux bdd l vis''
         _ -> vis''
     in
-      vis'''
-      
-    
+      vis'''    
 
--- test for build
+-- Test for build (figure 3, p. 11) 
 test0 = And (Eq (Var 1) (Var 2)) (Eq (Var 3) (Var 4))
+runTest0 = BDD.print $ build test0 4
 
--- test for apply
-test11 :: BDD
-test11 = (Map.fromList [(0, (6, Nothing, Nothing)), (1, (6, Nothing, Nothing)), (2, (5, Just 1, Just 0)), (3, (4, Just 2, Just 0)), (4, (4, Just 0, Just 2)), (5, (3, Just 3, Just 4)), (6, (2, Just 5, Just 0)), (7, (2, Just 0, Just 5)), (8, (1, Just 6, Just 7))],
-          Map.fromList  [((6, Nothing, Nothing), 0), ((6, Nothing, Nothing), 1), ((5, Just 1, Just 0), 2), ((4, Just 2, Just 0), 3), ((4, Just 0, Just 2), 4), ((3, Just 3, Just 4), 5), ((2, Just 5, Just 0), 6), ((2, Just 0, Just 5), 7), ((1, Just 6, Just 7), 8)]) 
+-- Test for apply (figure 12, p. 21)
+test11 :: BDDNode
+test11 = ((Map.fromList [(0, (6, Nothing, Nothing)), (1, (6, Nothing, Nothing)), (2, (5, Just 1, Just 0)), (3, (4, Just 2, Just 0)), (4, (4, Just 0, Just 2)), (5, (3, Just 3, Just 4)), (6, (2, Just 5, Just 0)), (7, (2, Just 0, Just 5)), (8, (1, Just 6, Just 7))],
+          Map.fromList  [((6, Nothing, Nothing), 0), ((6, Nothing, Nothing), 1), ((5, Just 1, Just 0), 2), ((4, Just 2, Just 0), 3), ((4, Just 0, Just 2), 4), ((3, Just 3, Just 4), 5), ((2, Just 5, Just 0), 6), ((2, Just 0, Just 5), 7), ((1, Just 6, Just 7), 8)]), 8) 
 
-test12 :: BDD
-test12 = (Map.fromList [(0, (6, Nothing, Nothing)), (1, (6, Nothing, Nothing)), (2, (5, Just 1, Just 0)), (3, (3, Just 2, Just 0)), (4, (3, Just 0, Just 2)), (5, (1, Just 3, Just 4))], Map.fromList [((6, Nothing, Nothing), 0), ((6, Nothing, Nothing), 1), ((5, Just 1, Just 0), 2), ((3, Just 2, Just 1), 3), ((3, Just 0, Just 2), 4), ((1, Just 3, Just 4), 5)])
+test12 :: BDDNode
+test12 = ((Map.fromList [(0, (6, Nothing, Nothing)), (1, (6, Nothing, Nothing)), (2, (5, Just 1, Just 0)), (3, (3, Just 2, Just 0)), (4, (3, Just 0, Just 2)), (5, (1, Just 3, Just 4))], Map.fromList [((6, Nothing, Nothing), 0), ((6, Nothing, Nothing), 1), ((5, Just 1, Just 0), 2), ((3, Just 2, Just 1), 3), ((3, Just 0, Just 2), 4), ((1, Just 3, Just 4), 5)]), 5)
 
--- test for build and apply
---test21 = build $ Eq (Var 1) (Var 2)
---test22 = build $ Eq (Var 3) (Var 4)
--- you would have to keep additionally the number of variables in each bdd
+runTest1 = BDD.print $ apply test11 test12 (&&)
 
-
--- test for restrict
-test3 = Or (Eq (Var 1) (Var 2)) (Var 3)
+-- Test for restrict (p. 20)
+test2 = Or (Eq (Var 1) (Var 2)) (Var 3)
+runTest2 = BDD.print $ restrict (build test2 3) 2 False
